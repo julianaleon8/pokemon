dded038f20ed2978d311b3d7ef4c4ce840860827
juli <juli@mercury.(none)>
2013-02-14 20:30:36 -0430

    casi

--- tarea.hs
+++ tarea.hs
@@ -1,24 +1,26 @@
-import List (insert)
+import Data.List 
 import Data.Maybe 
+import Data.Graph.Inductive.Internal.Heap(
+  Heap(..),findMin,deleteMin)
+  
 data ArbolRosa a = ArbolRosa { elemento :: a
                              ,hijos :: [ArbolRosa a]
                    }deriving (Show, Read)
-                             
+
+
 --(ArbolRosa 5 [ArbolRosa 6 [ArbolRosa 9 []]])
                                   
 --1)
+
 insertionSort :: (Ord a) => [a]-> [a]
 insertionSort  xs =  foldl (flip insert) [] xs    
 
-partition :: (a -> Bool) -> [a] -> ([a], [a])
---partition _ [] = ([],[]) 
---partition p (x:xs) = 
-partition p xs = foldr (select p) ([],[]) xs
-
-select :: (a -> Bool) -> a -> ([a], [a]) -> ([a], [a])
-select p x ~(ts,fs) | p x       = (x:ts,fs)
-                    | otherwise = (ts, x:fs)
-
+partition' :: (a -> Bool) -> [a] -> ([a], [a])
+partition' x xs = foldr (separar x) ([],[]) xs
+                 
+separar x b ~(a,c) | x b = (b:a,c) 
+                   | otherwise = (a,b:c)
+                    
 --2)
 
 altura :: ArbolRosa a -> Int
@@ -40,22 +42,38 @@ mapRosa f (ArbolRosa e xs) = ArbolRosa (f e) (map (mapRosa f) xs)
 
 esHeap :: Ord a => ArbolRosa a -> Bool
 esHeap (ArbolRosa e []) = True
-esHeap (ArbolRosa e (x:xs) ) =False
-                          
+esHeap (ArbolRosa y xs) = foldl (&&) True (map (y>=)( (concat) (map (aplanar) xs))) && (foldl (&&) True (map (esHeap) xs))
 
 
 --3)
 
---foldRosa f a (ArbolRosa e xs) = f e (foldl (f) a (concatMap (aplanar) xs))  
-foldRosa f a (ArbolRosa e xs) = foldr f e (map (foldRosa f a) xs) 
---alturaFold (ArbolRosa _ xs)=  foldRosa (uno) 0 xs
+--a) La funcion de plegado debe recibir una sola funcion puesto que solo posee un constructor para el tipo
+-- si la misma tendria varios constructiores deberia tener una funcion  para cada constructor del tipo 
+-- La firma de la funcion del plegado para esta (t1 -> [b] -> b) donde t1 es el tipo del ArbolRosa [b] es una lista de elementos   
+-- a los cuales se le aplico la funcion recursivamente y b el resultado de aplicar f a la lista
 
-sumaArbolFold = foldRosa (+) 0   
 
---aplanaFold (ArbolRosa a []) = [a] 
+--b) foldRosa :: (t1 -> [b] -> b) -> t -> ArbolRosa t1 -> b
+-- JUSTIFICACION 
+
+--foldRosa f a (ArbolRosa e xs) = f a (foldl (f) a (concatMap (aplanar) xs))  
+
+--foldRosa  f a (ArbolRosa e xs) = foldr f e (map (foldRosa f a) xs) 
+foldRosa :: (t1 -> [b] -> b) -> t -> ArbolRosa t1 -> b
+foldRosa  f a (ArbolRosa e xs) = f e (map (foldRosa f a) xs) 
+  
+alturaFold :: ArbolRosa a -> Int
+alturaFold a = foldRosa (\x y -> if null y then 0 else 1 + maximum y ) 0 a
+
+sumaArbolFold :: Num a => ArbolRosa a -> a
+sumaArbolFold a = foldRosa (\x y -> x + sum y ) 0 a   
+
+aplanaFold :: ArbolRosa a -> [a]
+aplanaFold ar@(ArbolRosa a xs) =  foldRosa (\x y -> x:concat y) [] ar
 
 
 --4)
+
 
 unfold' :: (b -> Maybe(a,b)) -> b -> [a]
 unfold' f x 
@@ -63,3 +81,18 @@ unfold' f x
 	| otherwise = fst (g):unfold' f (snd (g))
 	where 
 	g = fromJust(f x)
+
+
+tuplas :: [a]-> [(a,a)]
+tuplas []= []
+tuplas [a] = []
+tuplas (x:y:xs) = (x,y): tuplas xs
+
+aplanarUnfold :: ArbolRosa a -> [a] 
+aplanarUnfold a = unfoldr f [a]
+  where
+    f [] = Nothing 
+    f ((ArbolRosa e sa):xs) = Just (e,xs++sa)
+    
+
+
