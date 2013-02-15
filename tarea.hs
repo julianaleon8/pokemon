ece45b5737887d92cadaeb887ff9d06b248020ec
juli <juli@mercury.(none)>
2013-02-14 22:02:38 -0430

    listosssss toda

--- tarea.hs
+++ tarea.hs
@@ -1,7 +1,7 @@
 import Data.List 
 import Data.Maybe 
 import Data.Graph.Inductive.Internal.Heap(
-  Heap(..),findMin,deleteMin)
+  Heap(..),findMin,deleteMin, build, toList)
   
 data ArbolRosa a = ArbolRosa { elemento :: a
                              ,hijos :: [ArbolRosa a]
@@ -56,9 +56,10 @@ esHeap (ArbolRosa y xs) = foldl (&&) True (map (y>=)( (concat) (map (aplanar) xs
 --b) foldRosa :: (t1 -> [b] -> b) -> t -> ArbolRosa t1 -> b
 -- JUSTIFICACION 
 
---foldRosa f a (ArbolRosa e xs) = f a (foldl (f) a (concatMap (aplanar) xs))  
+foldRosa'' f a (ArbolRosa e xs) = f a (foldl (f) a (concatMap (aplanar) xs))  
 
---foldRosa  f a (ArbolRosa e xs) = foldr f e (map (foldRosa f a) xs) 
+--foldRosa' f a (ArbolRosa e xs) = f a foldr f e (map (foldRosa f a) xs) 
+
 foldRosa :: (t1 -> [b] -> b) -> t -> ArbolRosa t1 -> b
 foldRosa  f a (ArbolRosa e xs) = f e (map (foldRosa f a) xs) 
   
@@ -74,7 +75,7 @@ aplanaFold ar@(ArbolRosa a xs) =  foldRosa (\x y -> x:concat y) [] ar
 
 --4)
 
-
+--a) CHARARA DEBES JUSTIFICAR ESTOOOOO
 unfold' :: (b -> Maybe(a,b)) -> b -> [a]
 unfold' f x 
 	| isNothing (f x) = []
@@ -83,10 +84,40 @@ unfold' f x
 	g = fromJust(f x)
 
 
-tuplas :: [a]-> [(a,a)]
-tuplas []= []
-tuplas [a] = []
-tuplas (x:y:xs) = (x,y): tuplas xs
+
+
+factoriales :: Int -> [Int]
+factoriales = unfold' f
+	    where
+	    f x
+	      | x > 0	= Just (g x, x-1)
+	      | otherwise = Nothing
+	    g x = foldl (*) 1 [1..x]
+
+esDivisible :: Int -> Int-> Bool
+esDivisible n m = if m*m < n
+	      	  then
+			if mod n m == 0
+			then True
+			else
+			esDivisible n (m + 1)
+		else False
+
+
+nPrimos :: Int -> Int -> [Int]
+nPrimos n m = [a | a <- [n..m], a /= 1, not (esDivisible a 2)]
+
+factorizar :: Int -> [Int]
+factorizar n = 1:(unfold' f n)
+	   where     
+	   	     f x
+		       | x > 1	= Just (g x, div x (g x))
+	     	       | otherwise = Nothing
+		     g x = h [2..x] x
+		     h (y:ys) x
+		       | (mod x y == 0) = y
+		       | otherwise = h ys x
+
 
 aplanarUnfold :: ArbolRosa a -> [a] 
 aplanarUnfold a = unfoldr f [a]
@@ -96,3 +127,19 @@ aplanarUnfold a = unfoldr f [a]
     
 
 
+lista :: [(a,a)] -> [a]
+lista [] = []
+lista (x:xs) = fst x: lista xs
+
+tuplas :: [a]-> [(a,a)]
+tuplas [] = []
+tuplas (x:xs) = (x,x): tuplas xs
+
+heapSort' :: Ord a => [a] -> [a]
+heapSort' [] = []
+heapSort' a = lista (unfold' f (toList(build (tuplas a))))
+ where
+    f [] = Nothing 
+    f (a:ax)  = Just (a,ax)
+
+    
