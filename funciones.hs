47d4fec073e6698eb973cd09247500f1b0890bac
juli <juli@mercury.(none)>
2013-02-19 00:58:38 -0430

    a

--- funciones.hs
+++ funciones.hs
@@ -1,8 +1,3 @@
-type Nombre
-     = String
-type Vida 
-     = Int 
-       
 data TipoPokemon 
      = Bug
      | Dark
@@ -22,27 +17,82 @@ data TipoPokemon
      | Steel
      | Water
      deriving (Bounded,Eq,Enum,Read,Show)    
-data Ataque 
-     = Nombre Bool Int Int Int  
-     deriving (Show)
-data Estadistica
-     = Vida Int Int Int Int Int Int Int deriving (Show)
+data Ataque = Ataque { nombreAtaque :: String 
+                       , tipoAtaque :: TipoPokemon
+                       , fisico :: Bool 
+                       , pps :: Int 
+                       , maximoAta :: Int
+                       , poder :: Int
+                     } deriving (Show)
+                                
+                                
+data Estadistica = Estadistica { vida :: Int 
+                               , ataque :: Int 
+                               , defensa :: Int
+                               , ataqueEspecial :: Int
+                               , defensaEspecial :: Int  
+                               , velocidad :: Int 
+                               , valorIndividual :: Int 
+                               , valorEsfuerzo :: Int
+                               } deriving (Show)
 
-data Especie
-     = Int String TipoPokemon Estadistica String String 
-       deriving (Show)
-                
-data Monstruo 
-     = Especie String Int Vida Int 
-
+data Especie = Especie { numeroCatalogo :: Int
+                       , nombreEspecie :: String
+                       , tipoPokemon :: TipoPokemon
+                       , estadisti :: Estadistica
+                       , preEvolucion :: Maybe Int
+                       , evolucion :: Maybe Int
+                       } deriving (Show)
+                                  
+data Monstruo = Monstruo { especie :: Especie
+                         ,sobreNombre :: String
+                         ,nivel :: Int 
+                         ,hp :: Int
+                         ,ataques :: Ataque
+                         }deriving (Show)
+                                   
+relacionAtaqueTipo :: TipoPokemon      -- Tipo de ataque a determinar la relación.
+                   -> ( [TipoPokemon]  -- Tipos super efectivos a el (2x dano). 
+                      , [TipoPokemon]  -- Tipos resistentes a el (0.5x dano).
+                      , [TipoPokemon]  -- Tipos inmunes a el (0x dano).
+                      )
+relacionAtaqueTipo x
+  | Bug      <- x = ([Grass, Psychic, Dark], [Fighting, Flying, Poison, Ghost, Steel, Fire], [])
+  | Dark     <- x = ([Ghost, Psychic], [Fighting, Steel, Dark], [])
+  | Dragon   <- x = ([Dragon], [Steel], [])
+  | Electric <- x = ([Flying, Water], [Grass, Electric, Dragon], [Ground])
+  | Fighting <- x = ([Normal, Rock, Steel, Ice, Dark], [Flying, Poison, Bug, Psychic], [Ghost])
+  | Fire     <- x = ([Bug, Steel, Grass, Ice], [Rock, Fire, Water, Dragon], [])
+  | Flying   <- x = ([Fighting, Bug, Grass], [Rock, Steel, Electric], [])
+  | Ghost    <- x = ([Ghost, Psychic], [Steel, Dark], [Normal])
+  | Grass    <- x = ([Ground, Rock, Water], [Flying, Poison, Bug, Steel, Fire, Grass, Dragon], [])
+  | Ground   <- x = ([Poison, Rock, Steel, Fire, Electric], [Bug, Grass], [Flying])
+  | Ice      <- x = ([Flying, Ground, Grass, Dragon], [Steel, Fire, Water], [])
+  | Normal   <- x = ([], [Rock, Steel], [Ghost])
+  | Poison   <- x = ([Grass], [Poison, Ground, Rock, Ghost], [Steel])
+  | Psychic  <- x = ([Fighting, Poison], [Steel, Psychic], [Dark])
+  | Rock     <- x = ([Flying, Bug, Fire, Ice], [Fighting, Ground, Steel], [])
+  | Steel    <- x = ([Rock, Ice], [Steel, Fire, Water, Electric], [])
+  | Water    <- x = ([Ground, Rock, Fire], [Water, Grass, Dragon], [])
 
 maxHp base nivel = ((31 + (2*base)+ 63.75 +100)*nivel)/100 +10 
 
 estadisticas base nivel = ((31+(2*base)+ 31/4)* nivel)/100 +5
 
 
-dano nivel poder ataque defensa modi= ((( ( (2*nivel)/5 +2)*poder*(ataque/defensa)/50) ) )*modificador modi
+--dano pokemonAta pokemonDefensa numeroAtaque = ((( ( (2*nivel pokemonAta)/5 +2)*numeroAtaque*(ataques numeroAtaque/defensa pokemonDefensa)/50) ) )*modificador 
+-- modificador ( a d ) * modificador (a dd )  
+-- falta ponerlo para los dos tipos de defensa y multiplacarlo
+
+modificador ataque defensa  
+  | elem defensa (ptrestupla (relacionAtaqueTipo ataque)) = 2
+  | elem defensa (strestupla (relacionAtaqueTipo ataque)) = 0.5
+  | elem defensa (ttrestupla (relacionAtaqueTipo ataque)) = 0
+  | otherwise = 1
 
 
-modificador modi = 4
+ptrestupla (a,_,_)= a
+strestupla (_,a,_)= a
+ttrestupla (_,_,a)= a
+
 
